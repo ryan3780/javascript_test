61b82a62c2ab930105feed5261a1121222c368f0
Jocoos <jocoos@Jocoosui-iMac.local>
2019-10-07 18:35:35 +0900

    Update web

--- lotto.js
+++ lotto.js
@@ -1,21 +1,21 @@
 const lotArr = [];
 
 function compareNum(a, b) {
-  return a - b;
+    return a - b;
 }
 
 function makeNum() {
-  for (j = 0; j < 6; j++) {
-    for (i = 0; i < 6; i++) {
-      const randomNum = Math.floor(Math.random() * 45 + 1);
-      lotArr.push(randomNum);
+    for (j = 0; j < 6; j++) {
+        for (i = 0; i < 6; i++) {
+            const randomNum = Math.floor(Math.random() * 45 + 1);
+            lotArr.push(randomNum);
+        }
     }
-  }
 }
 makeNum();
 
 const blank = lotArr.map(element => {
-  return " " + element + " ";
+    return " " + element + " ";
 });
 
 const lottoNumberA = blank.slice(0, 6);
@@ -28,33 +28,33 @@ const lottoNumberE = blank.slice(24, 30);
 // console.log(a);
 
 function change(arr) {
-  const a = arr.sort(compareNum);
-  const aa = a.toString();
-  const aaa = aa.split(",").join("/");
-  return document.write(aaa);
+    const a = arr.sort(compareNum);
+    const aa = a.toString();
+    const aaa = aa.split(",").join("/");
+    return document.write(aaa);
 }
 
 // 필요 없는 부분이 있지 않나요?? 2번씩 체크를 한다거나??
 function check(arr) {
-  const base = [...new Set(arr)];
-  // console.log(base);
+    const base = [...new Set(arr)];
+    // console.log(base);
 
-  // 중복으로 인해서, 배열의 원소가 모자르면 채우는 조건문
-  if (base.length !== 6) {
-    const random = " " + Math.floor(Math.random() * 45 + 1) + " ";
-    const chRandom = random.toString();
-    const newNum = [...base, chRandom];
-    const lotto = [...newNum];
-    if (lotto.length === 5) {
-      const sRandom = " " + Math.floor(Math.random() * 45 + 1) + " ";
-      const str = sRandom.toString();
-      const realLotto = [...lotto, str];
-      return check(realLotto);
+    // 중복으로 인해서, 배열의 원소가 모자르면 채우는 조건문
+    if (base.length !== 6) {
+        const random = " " + Math.floor(Math.random() * 45 + 1) + " ";
+        const chRandom = random.toString();
+        const newNum = [...base, chRandom];
+        const lotto = [...newNum];
+        if (lotto.length === 5) {
+            const sRandom = " " + Math.floor(Math.random() * 45 + 1) + " ";
+            const str = sRandom.toString();
+            const realLotto = [...lotto, str];
+            return check(realLotto);
+        }
+        return check(lotto);
+    } else {
+        const chbase = [...base];
+        // console.log(chbase);
+        return change(chbase);
     }
-    return check(lotto);
-  } else {
-    const chbase = [...base];
-    // console.log(chbase);
-    return change(chbase);
-  }
 }
