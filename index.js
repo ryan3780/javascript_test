abdfdfb52c0f30cb07801151454856c3b0c229d3
ryan3780 <guemhong3780@gmail.com>
2019-10-23 23:10:35 +0900

    clone

--- index.js
+++ index.js
@@ -3,19 +3,92 @@ import { bro } from "./bro";
 import * as lib from "./chapter5";
 import * as noty from "./noty";
 
+
 console.log("index.js");
 main();
 
-export const bbb = lib.deleteUser;
-export const ccc = noty.checkNoty;
+export const deleteUser = lib.deleteUser;
+export const checkNoty = noty.checkNoty;
 
 document.querySelector("h1").textContent = bro(`Dev 성적표`);
 document.querySelector("h1").style.textAlign = "center";
 
 export function addNum() {
-  if (document.body.style.backgroundColor === "snow") {
-    document.body.style.backgroundColor = "#f3f3f3";
-  } else {
-    document.body.style.backgroundColor = "snow";
-  }
+    if (document.body.style.backgroundColor === "snow") {
+        document.body.style.backgroundColor = "#f3f3f3";
+    } else {
+        document.body.style.backgroundColor = "snow";
+    }
 }
+
+// https://marshall-ku.tistory.com/267 출처 // 예쁜 계산기
+
+
+const input = document.getElementsByClassName("input")[0],
+    tmp = document.getElementsByClassName("tmp")[0];
+
+
+
+function i_empty() {
+    input.innerText = ""
+}
+
+function t_empty() {
+    tmp.innerText = ""
+}
+
+(function getCalFunction() {
+    document.getElementsByClassName("all-clear")[0].addEventListener("click", function() {
+            i_empty(),
+                t_empty()
+        }),
+        document.getElementsByClassName("clear")[0].addEventListener("click", function() {
+            i_empty()
+        }),
+        Array.from(document.querySelectorAll('.number')).forEach(a => {
+            a.addEventListener('click', function() {
+                if (input.innerText.length <= 19) {
+                    input.innerText = input.innerText + this.innerText
+                    console.log(this.innerText)
+                } else {
+                    alert("최대 입력 범위를 초과했습니다!")
+                }
+            });
+        }),
+        document.getElementsByClassName("dot")[0].addEventListener("click", function() {
+            if (input.innerText.indexOf(".") === -1) {
+                input.innerText = input.innerText + "."
+            }
+        }),
+        document.getElementsByClassName("sign")[0].addEventListener("click", function() {
+            if (input.innerText.indexOf("-") === -1 && input.innerText !== "") {
+                input.innerText = "-" + input.innerText
+            } else {
+                input.innerText = input.innerText.replace("-", "")
+            }
+        }),
+        Array.from(document.querySelectorAll('.amt')).forEach(a => {
+            a.addEventListener('click', function() {
+                if (input.innerText !== "") {
+                    if (tmp.innerText === "") {
+                        tmp.innerText = input.innerText + " " + this.innerText
+                    } else {
+                        tmp.innerText = tmp.innerText + " " + input.innerText + " " + this.innerText
+                    }
+                }
+                if (input.innerText === "" && tmp.innerText !== "") {
+                    tmp.innerText = tmp.innerText.slice(0, -1) + " " + this.innerText
+                }
+
+                i_empty()
+            });
+        }),
+        document.getElementsByClassName("result")[0].addEventListener("click", function() {
+            if (input.innerText !== "") {
+                input.innerText = eval(tmp.innerText + input.innerText),
+                    t_empty()
+            }
+        });
+})()
+
+//git contributions...다 날아가는데 왜그런거지;;
--- noty.js
+++ noty.js
@@ -1,41 +1,71 @@
-const noty = document.getElementById("noty");
-noty.innerHTML = `<table class= 'table table-hover table-striped'>
-<thead>
-<tr>
-<th></th>
-<th></th>
-</tr>
-</thead>
-<tbody>
-<tr>
-<td id=sell>11시 주식 팔기</td>
-<td><button class = 'btn btn-success' onclick = index.ccc(0)>확인</button></td></tr>
-<tr><td id = buy>14시 주식 사기</td>
-<td><button class = 'btn btn-success' onclick = index.ccc(1)>확인</button></td></tr>
-</tbody>
-</table>`;
+const notyList = noticeList()
+
+function noticeList() {
+    return ["11시 주식 팔기", '14시 주식 사기']
+}
+
+
+
+function makeNotyToTable(list) {
+    const table = list.map(
+        (elm, index) => `<tr> 
+    <td id = ${index}>${elm}</td>
+    <td>
+    <div class="checks etrans" style="bottom: 15px;">
+    <input type = checkbox  id="ex_chk${index+3}" name = agree onclick = index.checkNoty(${index}) >
+    <label for="ex_chk${index+3}" ></label>
+    </input>
+    </td>
+    </tr>`
+    );
+    return table;
+}
+
+
+function showTable(table) {
+    const t = table.join("");
+    document.getElementById("noty").innerHTML = t;
+}
+
+function render() {
+    const list = makeNotyToTable(notyList);
+    showTable(list)
+}
+
+render()
 
 function checkTime() {
-  const date = new Date();
-  const time = date.getHours();
-  //   console.log(typeof time);
-  if (time === 11) {
-    confirm("Sell Stock");
-  }
-  if (time === 14) {
-    confirm("Buy Stock");
-  }
+    const date = new Date();
+    const time = date.getHours();
+    if (time === 11) {
+        confirm("Sell Stock");
+    }
+    if (time === 13) {
+        confirm("Buy Stock");
+    }
 }
 
 checkTime();
 
-export function checkNoty(n) {
-  const sell = document.getElementById("sell");
-  const buy = document.getElementById("buy");
+export function checkNoty() {
+    const zero = document.getElementById('0')
+    const one = document.getElementById('1')
+    const chk = document.getElementsByName('agree')
 
-  if (n === 0) {
-    sell.style.textDecoration = "line-through";
-  } else {
-    buy.style.textDecoration = "line-through";
-  }
+    if (chk[0].checked === true) {
+        zero.style.textDecoration = 'line-through'
+
+    }
+    if (chk[0].checked === false) {
+        zero.style.textDecoration = 'none'
+    }
+
+    if (chk[1].checked === true) {
+        one.style.textDecoration = 'line-through'
+
+    }
+    if (chk[1].checked === false) {
+        one.style.textDecoration = 'none'
+    }
+
 }
