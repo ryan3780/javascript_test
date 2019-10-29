98854b252d7ae7c9e0a9f28b3584c41ba567cc08
ryan3780 <guemhong3780@gmail.com>
2019-10-29 13:35:14 +0900

    Finish?? login and register??

--- login.js
+++ login.js
@@ -1,16 +1,20 @@
-// 관리자 ID와 비밀번호를 체크 하는 함수
-const data = JSON.parse(localStorage.getItem('items'))
+// 로컬 저장소에 키랑 값이 있으면, 가져오고, 없으면 'admin'만 들어 있는 배열이 itemsArray가 된다
+let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+// JSON형식으로 가져온다
+let data = JSON.parse(localStorage.getItem('items'));
 
+// 관리자 ID와 비밀번호를 체크 하는 함수
 function checkIdAndPassword() {
     let id = document.getElementById('id');
-    let password = document.getElementById('password')
+    let password = document.getElementById('password');
+    const create = document.getElementById('create');
     for (let k in data) {
         if (id.value === data[k].mail) {
             if (password.value === data[k].pw) {
-                return location.href = 'index.htm'
+                return location.href = 'http://localhost:1234/'
             }
         }
     }
-    return console.log("plz join")
+    return alert("가입 해주세요."), create.focus();
 
 }
--- register.js
+++ register.js
@@ -7,18 +7,23 @@ const checkSpace = /\s/g;
 //대,소문자, 특수기호 등 8자리만 허용하는 정규식
 const passwordRegEx = /^[a-z0-9A-Z\~\!\@\#\%\^\&\*\(\)\{\}\[\,\.\<\>\;\:\'\"\]\\\=\+\_\-]{8}$/g;
 
+//태그 id 변수로 선언
 const mail = document.getElementById('mail');
 const password = document.getElementById('password');
 const Repassword = document.getElementById('RePassword')
 
-let userMail = [{ mail: 'admin', pw: 'admin' }]
-localStorage.setItem('items', JSON.stringify(userMail))
-let data = JSON.parse(localStorage.getItem('items'))
+// 로컬 저장소에 키랑 값이 있으면, 가져오고, 없으면 'admin'만 들어 있는 배열이 itemsArray가 된다
+let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [{ mail: 'admin', pw: 'admin' }]
 
+// 로컬 저장소에 값을 넣어준다
+localStorage.setItem('items', JSON.stringify(itemsArray));
+// JSON형식으로 가져온다
+let data = JSON.parse(localStorage.getItem('items'));
 
+//data에 있는 값을 콘솔로 확인
 data.map((item) => {
     console.log(item)
-})
+});
 
 // localstorage에 가입자들의 메일과 비밀번호 저장하기
 function addMailAndPw() {
@@ -28,12 +33,11 @@ function addMailAndPw() {
     pw = password.value;
     newUser.mail = email;
     newUser.pw = pw
-    userMail.push(newUser)
-
-    localStorage.setItem('items', JSON.stringify(userMail))
+    itemsArray.push(newUser)
+    localStorage.setItem('items', JSON.stringify(itemsArray))
     let data = JSON.parse(localStorage.getItem('items'))
 
-    return userMail, console.log(data)
+    return itemsArray, console.log(data)
 }
 
 // 메일 형식, 비밀번호 형식 체크하는 함수
@@ -44,32 +48,32 @@ function checkingEmail(email) {
     re = Repassword.value
 
     if (email.search(checkSpace) === -1 && checkEmail.test(email) === true) {
-        checkingPassword(pw)
-        checkingRePassword(re)
-        addMailAndPw()
-        return console.log("사용 가능한 이메일 형식입니다 : " + email);
+        if (checkingPassword(pw) === true) {
+            if (checkingRePassword(re) === pw)
+                addMailAndPw()
+            return console.log("사용 가능한 이메일 형식입니다 : " + email), location.href = 'http://localhost:1234/';
+        }
     } else {
-        alert("이메일 형식이 아닙니다.")
-        return console.log("사용 불가한 이메일입니다.");
+        alert('입력 부탁드립니다.')
     }
-
 }
+
 
 // 비밀번호 체크 하는 함수 
 function checkingPassword(pw) {
     let result = passwordRegEx.test(pw);
     if (result) {
-        return console.log("사용 가능한 비밀번호 형식입니다 : " + pw);
+        return result;
     } else {
-        return console.log("사용 불가한 비밀번호 입니다.");
+        return alert("사용 불가한 비밀번호 입니다.");
     }
 }
 // 입력한 비밀번호가 같은 값인지, 체크 하는 함수
 function checkingRePassword(re) {
     if (re === pw) {
-        return console.log("same")
+        return re
     } else {
-        return alert('비밀번호가 일치하지 않습니다.')
+        return alert('비밀번호가 일치하지 않습니다.');
     }
 
 }
