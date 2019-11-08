d202e5d35f8bc2da1f60e3e9d4aba3524145e801
ryan3780 <guemhong3780@gmail.com>
2019-11-08 23:37:55 +0900

    React React React

--- phone-book/src/App.js
+++ phone-book/src/App.js
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import PhoneForm from "./components/PhoneForm";
+import PhoneInfoList from "./components/PhoneInfoList";
 
-function App() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <PhoneForm />
-    </div>
-  );
+class App extends Component {
+  id = 2;
+  state = {
+    information: [
+      {
+        id: 0,
+        name: "김민준",
+        phone: "010-0000-0000"
+      },
+      {
+        id: 1,
+        name: "홍길동",
+        phone: "010-0000-0001"
+      }
+    ]
+  };
+  handleCreate = data => {
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({ id: this.id++, ...data })
+    });
+  };
+  handleRemove = id => {
+    const { information } = this.state;
+    this.setState({
+      information: information.filter(info => info.id !== id)
+    });
+  };
+  render() {
+    // const { information } = this.state;
+    return (
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <PhoneForm onCreate={this.handleCreate} />
+        <PhoneInfoList
+          data={this.state.information}
+          onRemove={this.handleRemove}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- phone-book/src/components/PhoneForm.js
+++ phone-book/src/components/PhoneForm.js
@@ -5,31 +5,51 @@ class PhoneForm extends Component {
     name: "",
     phone: ""
   };
+
   handleChange = e => {
+    let nameLength = e.target.value.length;
+    console.log(nameLength);
     this.setState({
       [e.target.name]: e.target.value
     });
   };
 
+  handleSubmit = e => {
+    e.preventDefault();
+    let nameLength = e.target.name.value.length;
+    if (nameLength < 2) {
+      return this.setState({
+        name: "2글자 이상",
+        phone: "형식에 맞게"
+      });
+    }
+    this.props.onCreate(this.state);
+    // console.log(this.props.onCreate);
+    this.setState({
+      name: "",
+      phone: ""
+    });
+  };
+
   render() {
+    // console.log(this.state);
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <input
           placeholder="이름"
           value={this.state.name}
           onChange={this.handleChange}
           name="name"
+          required
         />
         <input
           placeholder="전화번호"
           value={this.state.phone}
           onChange={this.handleChange}
           name="phone"
+          required
         />
-        <div>
-          {this.state.name}
-          {this.state.phone}
-        </div>
+        <button type="submit">등록</button>
       </form>
     );
   }
